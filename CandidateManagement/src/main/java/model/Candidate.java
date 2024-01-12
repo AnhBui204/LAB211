1735e36412f4eb41d9d7cfc086ef2130b9bd253a
AnhBui204 <155693129+AnhBui204@users.noreply.github.com>
2024-01-12 16:45:04 +0700

    Update Candidate.java

--- CandidateManagement/src/main/java/model/Candidate.java
+++ CandidateManagement/src/main/java/model/Candidate.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author XUAN LINH
- */
 public class Candidate {
     private int candidateID;
    private String firstName;
