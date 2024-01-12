c54e1561a39328d43a0cacdb3b3ffe5ae3848136
AnhBui204 <155693129+AnhBui204@users.noreply.github.com>
2024-01-12 16:44:29 +0700

    Create Main.java

---
+++ CandidateManagement/src/main/java/Main/Main.java
@@ -0,0 +1,11 @@
+
+package Main;
+
+import Controller.CandidateManager;
+
+public class Main {
+    public static void main(String[] args) {
+        CandidateManager manager=new CandidateManager();
+        manager.runProgram();
+    }
+}
