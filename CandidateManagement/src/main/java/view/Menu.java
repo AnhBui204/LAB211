cda389d6c1648972910e7dc54e98df9059c365d8
AnhBui204 <155693129+AnhBui204@users.noreply.github.com>
2024-01-12 16:36:39 +0700

    Menu.java

--- CandidateManagement/src/main/java/view/Menu.java
+++ CandidateManagement/src/main/java/view/Menu.java
@@ -1,1 +1,11 @@
+pakage view;
 
+ private void displayMainMenu(){
+         System.out.println("HE THONG QUAN LY UNG VIEN");
+         System.out.println("1. Experience");
+         System.out.println("2.Fresher");
+         System.out.println("3.Intern");
+         System.out.println("4.Search");
+         System.out.println("5.Exit");
+         System.out.println("Vui long chon chuc nang(1-5): ");
+     }
