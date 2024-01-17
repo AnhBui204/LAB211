a8d3f16cdd832b0e94e0be1f713ac6884581bd16
AnhBui204 <155693129+AnhBui204@users.noreply.github.com>
2024-01-17 11:35:32 +0700

    Update StudentManage.java

--- StudentManage/src/studentmanage/StudentManage.java
+++ StudentManage/src/studentmanage/StudentManage.java
@@ -6,7 +6,6 @@ public class StudentManage {
 
     public static void main(String[] args) {
         String[] choice = {"Create","Find and Sort","Update/Delete","Report","Exit"};
-        new StudentView.run();
+        new StudentView().run();
     }
-    
 }
