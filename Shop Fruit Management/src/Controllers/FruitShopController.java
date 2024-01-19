5bff5345e0bea08a4d3ece7493abf4e7993e28fe
linhnguyenxuan0201 <155693517+linhnguyenxuan0201@users.noreply.github.com>
2024-01-19 12:40:50 +0700

    Update FruitShopController.java

--- Shop Fruit Management/src/Controllers/FruitShopController.java
+++ Shop Fruit Management/src/Controllers/FruitShopController.java
@@ -26,6 +26,52 @@ public class FruitController {
 
         fruitList.add(a);
     }
+    public void Shopping(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Danh Sach Trai Cay: ");
+        displayFruit();
+        System.out.println("Ban da chon: ");
+        int selectedFruitId=sc.nextInt();
+        // tim trai cay duoc chon
+        Fruit selectedFruit=null;
+        for(Fruit fruit: fruits){
+            if(fruit.getId()==selectedFruitId){
+                selectedFruit=fruit;
+                break;
+            }
+        }
+        if(selectedFruit !=null){
+            System.out.println("Vui long nhap so luong(con " + selectedFruit.getQuanity()+"Trai cay: ");
+            int quanity=sc.nextInt();
+            if(quanity>selectedFruit.getQuanity()){
+                System.out.println("So luong trai cay khong du !");
+                return;
+            }
+            System.out.println("Ban co muon dat hang hay khong (Y/N)");
+            char choice=sc.next().charAt(0);
+            if(choice == 'Y'||choice =='y'){
+                double total= quanity * selectedFruit.getPrice();
+                System.out.println("San Pham |So Luong |Gia|Tong Cong");
+                System.out.println(selectedFruit.getName()+" "+quanity+" "+selectedFruit.getPrice()+"$"+total+"$");
+                System.out.println("Nhap ten cua ban: ");
+                String customerName=sc.next();
+                //luu vao Hastable
+                 ArrayList<Fruit> orderList = orders.getOrDefault(customerName, new ArrayList<>());
+                orderList.add(new Fruit(selectedFruit.getId(), selectedFruit.getName(),
+                        selectedFruit.getPrice(), quanity, selectedFruit.getOrgin()));
+                orders.put(customerName, orderList);
+
+                // Cập nhật số lượng trái cây còn lại trong cửa hàng
+                selectedFruit.setQuanity(selectedFruit.getQuanity()- quanity );
+
+                System.out.println("Đặt hàng thành công!");
+            } else {
+                System.out.println("Quay lại danh sách trái cây để tiếp tục đặt hàng.");
+            }
+        } else {
+            System.out.println("Trái cây không tồn tại!");
+        }
+            }
 
     public void checkYN() {
     while (true) {
