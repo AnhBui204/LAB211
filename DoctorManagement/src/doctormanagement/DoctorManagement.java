20f8b7eb3f3c609ae9223107d4af2c11e551df0a
AnhBui204 <155693129+AnhBui204@users.noreply.github.com>
2024-02-02 08:00:43 +0700

    Update DoctorManagement.java

--- DoctorManagement/src/doctormanagement/DoctorManagement.java
+++ DoctorManagement/src/doctormanagement/DoctorManagement.java
@@ -1,10 +1,10 @@
 package doctormanagement;
 
-import View.DoctorView;
+import Controller.DoctorController;
 public class DoctorManagement {
 
     public static void main(String[] args) {
-        new DoctorView().run();
+        new DoctorController().run();
     }
     
 }
