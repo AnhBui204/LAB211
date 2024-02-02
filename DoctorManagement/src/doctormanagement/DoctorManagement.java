85d9501464b10e6cfed01fc82f0ae2d9cb03a265
AnhBui204 <155693129+AnhBui204@users.noreply.github.com>
2024-02-02 07:31:06 +0700

    Add files via upload

---
+++ DoctorManagement/src/doctormanagement/DoctorManagement.java
@@ -0,0 +1,10 @@
+package doctormanagement;
+
+import View.DoctorView;
+public class DoctorManagement {
+
+    public static void main(String[] args) {
+        new DoctorView().run();
+    }
+    
+}
